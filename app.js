a5dcc3aa803778eb76d923e9694431fdadf3749d
mrghost077 <msnlms86@gmail.com>
2024-11-30 10:10:19 +0530

    added the function to put symbol on cells

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const gameBoard = document.querySelector("#gameBoard");
 const gameInfo = document.querySelector('#gameInfo');
 const cells = ["","","","","","","","","",];
+let playTurn = "circle";
 
 gameInfo.textContent = 'Circle goes first';
 
 function board ()
 {
-    cells.forEach((cell, index) =>
+    cells.forEach((_cell, index) =>
     {
         const cellElement = document.createElement("div");
         cellElement.classList.add("square");
-        
+        cellElement.id = index
+        cellElement.addEventListener("click", addSymbol);
         gameBoard.append(cellElement);
     })
 }
 
 board();
+
+function addSymbol (e)
+{
+    console.log("clicked", e.target);
+    const cellSymbol = document.createElement("div");
+    cellSymbol.classList.add(playTurn);
+    playTurn = playTurn === "circle" ? "cross" : "circle";
+    gameInfo.textContent = "Now it is " + playTurn +"'s turn !";
+    e.target.removeEventListener("click", addSymbol);
+
+    e.target.append(cellSymbol);
+}
