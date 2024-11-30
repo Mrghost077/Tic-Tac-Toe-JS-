620aa056ccae2e710d8c6d3f9db5fa8e69511778
mrghost077 <msnlms86@gmail.com>
2024-11-30 10:43:26 +0530

    Added game algorithm to win

--- app.js
+++ app.js
@@ -27,6 +27,43 @@ function addSymbol (e)
     playTurn = playTurn === "circle" ? "cross" : "circle";
     gameInfo.textContent = "Now it is " + playTurn +"'s turn !";
     e.target.removeEventListener("click", addSymbol);
-
     e.target.append(cellSymbol);
+
+    calculateScore();
+
 }
+
+function calculateScore ()
+{
+    const allSquares = document.querySelectorAll(".square");
+    const winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+    winningCombinations.forEach(array =>{
+       const circleWin =  array.every(cell => 
+            allSquares[cell].firstChild?.classList.contains("circle"));
+
+            if (circleWin)
+                {
+                    gameInfo.textContent = "Circle Wins the Game !!!"
+                    /* once the game is ended (a symbol wins) we have to stop the game from proceeding anymore
+                    for that we will have to remove event listeners from allthe square which wont work. so instaed
+                    we clone square and place it above the existing square so the game would end*/
+
+                    allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+                }
+        })
+
+        winningCombinations.forEach(array =>{
+            const crossWin =  array.every(cell => 
+                 allSquares[cell].firstChild?.classList.contains("cross"));
+     
+                 if (crossWin)
+                     {
+                         gameInfo.textContent = "Cross Wins the Game !!!";
+                         allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+                     }
+             })
+        
+       
+}
+
